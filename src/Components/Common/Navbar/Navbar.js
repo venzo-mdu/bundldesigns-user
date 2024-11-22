8ab75f8eed8f6f788fda3b4ea8b33174190659ee
Sushmitha3403 <sushmithacu2@gmail.com>
2024-11-22 17:04:46 +0530

    navbar changes

--- src/Components/Common/Navbar/Navbar.js
+++ src/Components/Common/Navbar/Navbar.js
@@ -1,4 +1,4 @@
-import React from 'react'
+/*import React from 'react'
 import '../Navbar/Navbar.css'
 import NavLogo from '../../../Images/Navbar/Navlogo.svg'
 import Search from '../../../Images/Navbar/searchicon.svg'
@@ -30,3 +30,51 @@ export const Navbar = () => {
     </div>
   )
 }
+*/
+
+import React from 'react';
+import '../Navbar/Navbar.css';
+import NavLogo from '../../../Images/Navbar/Navlogo.svg';
+import Search from '../../../Images/Navbar/searchicon.svg';
+import Cart from '../../../Images/Navbar/cart.svg';
+import User from '../../../Images/Navbar/usericon.svg';
+import { NavLink } from 'react-router-dom';
+
+export const Navbar = () => {
+  return (
+    <div className="common-navbar">
+      <div className="nav-logo">
+        <img src={NavLogo} alt="Navigation Logo" />
+      </div>
+
+      <div className="nav-items">
+        <p>
+          <NavLink style={{ color: '#000000' }} to="/">
+            Home
+          </NavLink>
+        </p>
+        <p>
+          <a className="text-black" href="/aboutus">
+            About
+          </a>
+        </p>
+        <p>
+          <a className="text-black" href="/our-work">
+            Work
+          </a>
+        </p>
+        <p>Bundls</p>
+      </div>
+
+      <div className="profile">
+        <input className="profile-input" placeholder="Search..." />
+        <img className="search-icon" src={Search} alt="Search Icon" />
+        <NavLink to="/mycart">
+          <img className="cart-icon" src={Cart} alt="Cart Icon" />
+        </NavLink>
+        <img className="user-icon" src={User} alt="User Icon" />
+        <div className="profile-name">HS</div>
+      </div>
+    </div>
+  );
+};
