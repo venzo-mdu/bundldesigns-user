feaf5759de19e600f2b6345f70c499c18da8e42f
suriyamurugan157 <suriyamurugan157@gmail.com>
2025-05-12 12:02:08 +0530

    font style Updated

--- src/Components/Common/Accordian.js
+++ src/Components/Common/Accordian.js
@@ -599,7 +599,7 @@ export const Accordian = ({ accordianTitle, addOnPayload,extraQty, bundlePackage
                 border:'none'
               }}
             >
-              <Typography className='!font-[700] !text-[18px]'>{isLang === 'ar' ? addOnData?.designs_details?.[title]?.name_arabic : title}</Typography>
+              <Typography className='!font-[700] !text-[22px]'>{isLang === 'ar' ? addOnData?.designs_details?.[title]?.name_arabic : title}</Typography>
             </AccordionSummary>
             <AccordionDetails >
               <Typography>
