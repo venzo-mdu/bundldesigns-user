0bd580dcee4d0846b90726e40f6b000a4e60f99c
Thavaseelanvenzo <thavaseelanr@venzotechnologies.com>
2025-06-08 15:36:42 +0530

    fix:uploaded content file uploaded fix added

--- src/Components/Pages/UploadContent.js
+++ src/Components/Pages/UploadContent.js
@@ -624,7 +624,7 @@ const fileName = e.target.files[0]?.name || "";
                     saveContent={saveContent}
                   />
 
-                  {/* <BundlOrder
+                   <BundlOrder
                     order={order}
                     skipId={skipId}
                     lang={lang}
@@ -635,7 +635,7 @@ const fileName = e.target.files[0]?.name || "";
                     uploadIcon={uploadIcon}
                     setSkipId={setSkipId}
                     saveContent={saveContent}
-                  /> */}
+                  /> 
                 </>
               )}
             </div>
