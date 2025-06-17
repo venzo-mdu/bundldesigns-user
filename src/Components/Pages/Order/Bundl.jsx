cc8f671049074707fe1d90ada8823791e54daa08
Thavaseelanvenzo <thavaseelanr@venzotechnologies.com>
2025-06-17 11:07:37 +0530

    fix:arabic space issue fixed

--- src/Components/Pages/Order/Bundl.jsx
+++ src/Components/Pages/Order/Bundl.jsx
@@ -56,7 +56,7 @@ function BundlOrder({
                         !hasMultipleQty
                           ? ""
                           : ""
-                      } pl-[5%] space-x-2 mt-[2%]`}
+                      } ${lang === "ar" ? "mr-8" : "text-left"} pl-[5%] space-x-2 mt-[2%]`}
                     >
                       <div className="-ml-[5%] w-[calc(100%+5%)] border-y border-black py-2">
                         <div className="pl-[5%]">
